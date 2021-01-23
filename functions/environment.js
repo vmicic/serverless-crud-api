8037eefa8f50b2913a3ec879d91fae96f0e70a89
vmicic <vukasin.micic5@gmail.com>
2021-01-23 16:24:40 +0100

    Change env schema

--- functions/environment.js
+++ functions/environment.js
@@ -26,32 +26,52 @@ const errorResponse = (event, message) => ({
   ),
 });
 
-const createEnv = async (environment) => {
+const environmentSchema = new mongoose.Schema({
+  name: String,
+  entities: [],
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  environments: [environmentSchema],
+});
+
+const createEnv = async (environmentName) => {
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     const conn = mongoose.connection;
 
-    const User = conn.model(
-      "User",
-      new mongoose.Schema({ username: String, environments: [String] })
-    );
+    const Environment = conn.model("Environment", environmentSchema);
 
-    const query = { username: "ghost", environments: { $nin: environment } };
+    const User = conn.model("User", userSchema);
+
+    const environment = new Environment({
+      name: environmentName,
+      entities: [],
+    });
+
+    const query = {
+      username: "ghost",
+      "environments.name": { $ne: environmentName },
+    };
+
     User.findOneAndUpdate(
       query,
       { $push: { environments: environment } },
       { useFindAndModify: false },
       function (err) {
+        console.log(err);
         if (err) return handleError(err);
       }
     );
   } catch (error) {
-    return errorResponse(environment, "Error!");
+    return errorResponse(environmentName, "Error!");
   }
-  return successResponse(environment, "Success!");
+  return successResponse(environmentName, "Success!");
 };
 
 module.exports = { createEnv, successResponse, errorResponse };
