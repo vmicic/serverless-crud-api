6fcaa839906942a5d0a5d73be758dc087411fc24
vmicic <vukasin.micic5@gmail.com>
2021-01-23 15:18:32 +0100

    Add constraints when adding new env
    Don't add new env if one with same name exists

--- functions/environment.js
+++ functions/environment.js
@@ -26,30 +26,32 @@ const errorResponse = (event, message) => ({
   ),
 });
 
-const createEnv = async (data) => {
+const createEnv = async (environment) => {
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     const conn = mongoose.connection;
+
     const User = conn.model(
       "User",
       new mongoose.Schema({ username: String, environments: [String] })
     );
-    const query = { username: "ghost" };
+
+    const query = { username: "ghost", environments: { $nin: environment } };
     User.findOneAndUpdate(
       query,
-      { $push: { environments: data } },
+      { $push: { environments: environment } },
       { useFindAndModify: false },
       function (err) {
         if (err) return handleError(err);
       }
     );
   } catch (error) {
-    return errorResponse(data, "Error!");
+    return errorResponse(environment, "Error!");
   }
-  return successResponse(data, "Success!");
+  return successResponse(environment, "Success!");
 };
 
 module.exports = { createEnv, successResponse, errorResponse };
