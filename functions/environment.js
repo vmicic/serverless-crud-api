239cc1ae4e449df288bff6cbec76efe592f06bc6
vmicic <vukasin.micic5@gmail.com>
2021-01-23 19:27:01 +0100

    Add eslint config

--- functions/environment.js
+++ functions/environment.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/serverless-crud";
+const uri = 'mongodb://localhost:27017/serverless-crud';
 
 const successResponse = (event, message) => ({
   statusCode: 200,
   body: JSON.stringify(
     {
       message,
-      input: event,
+      input: event
     },
     null,
     2
-  ),
+  )
 });
 
 const errorResponse = (event, message) => ({
@@ -19,84 +19,88 @@ const errorResponse = (event, message) => ({
   body: JSON.stringify(
     {
       message,
-      input: event,
+      input: event
     },
     null,
     2
-  ),
+  )
 });
 
 const environmentSchema = new mongoose.Schema({
   name: String,
-  entities: [],
+  entities: []
 });
 
 const userSchema = new mongoose.Schema({
   username: String,
-  environments: [environmentSchema],
+  environments: [environmentSchema]
 });
 
-const createEnv = async (event, context, callback) => {
-  environmentName = event;
+const createEnv = async (event) => {
+  let environmentName = event;
 
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
 
     const conn = mongoose.connection;
 
-    const Environment = conn.model("Environment", environmentSchema);
+    const Environment = conn.model('Environment', environmentSchema);
 
-    const User = conn.model("User", userSchema);
+    const User = conn.model('User', userSchema);
 
     const environment = new Environment({
       name: environmentName,
-      entities: [],
+      entities: []
     });
 
     const query = {
-      username: "ghost",
-      "environments.name": { $ne: environmentName },
+      username: 'ghost',
+      'environments.name': { $ne: environmentName }
     };
 
     User.findOneAndUpdate(
       query,
       { $push: { environments: environment } },
       { useFindAndModify: false },
-      function (err) {
-        if (err) return handleError(err);
+      function callback(err) {
+        if (err) return console.log(err);
+        return 0;
       }
     );
   } catch (error) {
-    return errorResponse(environmentName, "Error!");
+    return errorResponse(environmentName, 'Error!');
   }
-  return successResponse(environmentName, "Success!");
+  return successResponse(environmentName, 'Success!');
 };
 
-const getEnv = async (event, context, callback) => {
-  environmentName = event;
+const getEnv = async (event) => {
+  var environmentName = event;
 
   await mongoose.connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   });
   const conn = mongoose.connection;
 
-  const User = conn.model("User", userSchema);
+  const User = conn.model('User', userSchema);
 
-  const query = { username: "ghost", "environments.name": environmentName };
-  User.findOne(query, function (err, doc) {
-    if (err) return handleError(err);
-    if (doc !== null) {
-      var environment = doc.environments.filter(function (el) {
-        return el.name === environmentName;
-      });
-      var entities = environment[0].entities;
-      return successResponse(entities, "Success");
-    }
+  const query = { username: 'ghost', 'environments.name': environmentName };
+  const doc = await User.findOne(query).exec();
+
+  const environment = doc.environments.filter(function callback(el) {
+    return el.name === environmentName;
   });
+
+  const entities = environment[0].entities;
+  return entities;
 };
 
-module.exports = { createEnv, getEnv, successResponse, errorResponse };
+module.exports = {
+  createEnv,
+  getEnv,
+  successResponse,
+  errorResponse
+};
